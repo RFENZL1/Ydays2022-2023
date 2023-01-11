42cc11a0ab4b067e7dcd5b8d16903123a88b2804
RFENZL <rfenzl@DESKTOP-TET0DOK.localdomain>
2023-01-11 16:35:51 +0100

    page des cartes fini

--- stylesheet.js
+++ stylesheet.js
@@ -4,32 +4,31 @@ function getRandomInt(max) {
   
   
   const div =()=>{
-    let nombrealeatoire= getRandomInt(0);
+    let nombrealeatoire= getRandomInt(4);
     console.log(nombrealeatoire)
 switch(nombrealeatoire){
       case 0:
-        document.querySelector(".back").style.backgroundImage = "url(/image/Dos-de-carte.jpg)";
+        document.querySelector(".back").style.backgroundImage = "url(/image/Harmonie-1.jpg)";
     console.log(0)
     break
       case 1:
-   
+         document.querySelector(".back").style.backgroundImage = "url(/image/Harmonie-2.jpg)";
     console.log(1)
     break
       case 2:
-   
+         document.querySelector(".back").style.backgroundImage = "url(/image/Harmonie-3.jpg)";
     console.log(2)
     break
       case 3:
-   
+         document.querySelector(".back").style.backgroundImage = "url(/image/Harmonie-4.jpg)";
     console.log(3)
     break
       case 4:
-   
+         document.querySelector(".back").style.backgroundImage = "url(/image/Harmonie.jpg)";
     console.log(4)
     break
       case 5:
-         
-   
+         document.querySelector(".back").style.backgroundImage = "url(/image/phototest.jpg)";   
     console.log(5)
     break
       case 6:
@@ -116,4 +115,236 @@ switch(nombrealeatoire){
    break
     }
 }
+const div2 =()=>{
+   let nombrealeatoire2= getRandomInt(4);
+   console.log(nombrealeatoire2)
+   if(nombrealeatoire2 == div.nombrealeatoire || nombrealeatoire2 == div3.nombrealeatoire3){
+      div2();
+   }
+switch(nombrealeatoire2){
+     case 0:
+       document.querySelector("#pannelId").style.backgroundImage = "url(/image/Harmonie-1.jpg)";
+   console.log(0)
+   break
+     case 1:
+        document.querySelector("#pannelId").style.backgroundImage = "url(/image/Harmonie-2.jpg)";
+   console.log(1)
+   break
+     case 2:
+        document.querySelector("#pannelId").style.backgroundImage = "url(/image/Harmonie-3.jpg)";
+   console.log(2)
+   break
+     case 3:
+        document.querySelector("#pannelId").style.backgroundImage = "url(/image/Harmonie-4.jpg)";
+   console.log(3)
+   break
+     case 4:
+        document.querySelector("#pannelId").style.backgroundImage = "url(/image/Harmonie.jpg)";
+   console.log(4)
+   break
+     case 5:
+        document.querySelector("#pannelId").style.backgroundImage = "url(/image/phototest.jpg)";   
+   console.log(5)
+   break
+     case 6:
+        
+  
+   console.log(6)
+   break
+     case 7:
+        
+  
+   console.log(7)
+   break
+     case 8:
+        
+  
+   console.log(8)
+   break
+     case 9:
+        
+  
+   console.log(9)
+   break
+     case 10:
+        
+  
+   console.log(10)
+   break
+     case 11:
+        
+  
+   console.log(11)
+   break
+     case 12:
+        
+  
+   console.log(12)
+   break
+     case 13:
+        
+  break
+     case 14:
+        
+  break
+     case 15:
+        
+  break
+     case 16:
+        
+  break
+     case 17:
+        
+  break
+     case 18:
+        
+  break
+     case 19:
+        
+  break
+     case 20:
+        
+  break
+     case 21:
+        
+  break
+     case 22:
+        
+  break
+     case 23:
+        
+  break
+     case 24:
+        
+  break
+     case 25:
+        
+  break
+     case 26: 
+  break
+     case 27:  
+  break
+     case 28: 
+  break
+     case 29: 
+  break
+   }
+}
+const div3 =()=>{
+   let nombrealeatoire3= getRandomInt(4);
+   if(nombrealeatoire3 == div.nombrealeatoire || nombrealeatoire3 == div2.nombrealeatoire2){
+      div3();
+   }
+   console.log(nombrealeatoire3)
+switch(nombrealeatoire3){
+     case 0:
+       document.querySelector("#card2").style.backgroundImage = "url(/image/Harmonie-1.jpg)";
+   console.log(0)
+   break
+     case 1:
+        document.querySelector("#card2").style.backgroundImage = "url(/image/Harmonie-2.jpg)";
+   console.log(1)
+   break
+     case 2:
+        document.querySelector("#card2").style.backgroundImage = "url(/image/Harmonie-3.jpg)";
+   console.log(2)
+   break
+     case 3:
+        document.querySelector("#card2").style.backgroundImage = "url(/image/Harmonie-4.jpg)";
+   console.log(3)
+   break
+     case 4:
+        document.querySelector("#card2").style.backgroundImage = "url(/image/Harmonie.jpg)";
+   console.log(4)
+   break
+     case 5:
+        document.querySelector("#card2").style.backgroundImage = "url(/image/phototest.jpg)";   
+   console.log(5)
+   break
+     case 6:
+        
+  
+   console.log(6)
+   break
+     case 7:
+        
+  
+   console.log(7)
+   break
+     case 8:
+        
+  
+   console.log(8)
+   break
+     case 9:
+        
+  
+   console.log(9)
+   break
+     case 10:
+        
+  
+   console.log(10)
+   break
+     case 11:
+        
+  
+   console.log(11)
+   break
+     case 12:
+        
+  
+   console.log(12)
+   break
+     case 13:
+        
+  break
+     case 14:
+        
+  break
+     case 15:
+        
+  break
+     case 16:
+        
+  break
+     case 17:
+        
+  break
+     case 18:
+        
+  break
+     case 19:
+        
+  break
+     case 20:
+        
+  break
+     case 21:
+        
+  break
+     case 22:
+        
+  break
+     case 23:
+        
+  break
+     case 24:
+        
+  break
+     case 25:
+        
+  break
+     case 26: 
+  break
+     case 27:  
+  break
+     case 28: 
+  break
+     case 29: 
+  break
+   }
+}
 div()
+div2()
+div3()
