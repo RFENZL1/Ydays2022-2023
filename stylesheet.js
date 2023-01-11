04f70442b8224ff658cdccee5a1318d55e6b7a82
RFENZL <rfenzl@DESKTOP-TET0DOK.localdomain>
2023-01-11 10:06:27 +0100

    animation des cartes faites

---
+++ stylesheet.js
@@ -0,0 +1,119 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  
+  const div =()=>{
+    let nombrealeatoire= getRandomInt(0);
+    console.log(nombrealeatoire)
+switch(nombrealeatoire){
+      case 0:
+        document.querySelector(".back").style.backgroundImage = "url(/image/Dos-de-carte.jpg)";
+    console.log(0)
+    break
+      case 1:
+   
+    console.log(1)
+    break
+      case 2:
+   
+    console.log(2)
+    break
+      case 3:
+   
+    console.log(3)
+    break
+      case 4:
+   
+    console.log(4)
+    break
+      case 5:
+         
+   
+    console.log(5)
+    break
+      case 6:
+         
+   
+    console.log(6)
+    break
+      case 7:
+         
+   
+    console.log(7)
+    break
+      case 8:
+         
+   
+    console.log(8)
+    break
+      case 9:
+         
+   
+    console.log(9)
+    break
+      case 10:
+         
+   
+    console.log(10)
+    break
+      case 11:
+         
+   
+    console.log(11)
+    break
+      case 12:
+         
+   
+    console.log(12)
+    break
+      case 13:
+         
+   break
+      case 14:
+         
+   break
+      case 15:
+         
+   break
+      case 16:
+         
+   break
+      case 17:
+         
+   break
+      case 18:
+         
+   break
+      case 19:
+         
+   break
+      case 20:
+         
+   break
+      case 21:
+         
+   break
+      case 22:
+         
+   break
+      case 23:
+         
+   break
+      case 24:
+         
+   break
+      case 25:
+         
+   break
+      case 26: 
+   break
+      case 27:  
+   break
+      case 28: 
+   break
+      case 29: 
+   break
+    }
+}
+div()
